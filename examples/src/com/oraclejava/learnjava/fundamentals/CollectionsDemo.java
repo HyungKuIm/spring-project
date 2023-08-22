092b55d96d3580205a4cb377bb5ca32d7255d8fb
HyungKu Im <damansa1@naver.com>
2023-08-22 19:03:09 +0900

    collection

--- examples/src/com/oraclejava/learnjava/fundamentals/CollectionsDemo.java
+++ examples/src/com/oraclejava/learnjava/fundamentals/CollectionsDemo.java
@@ -1,7 +1,10 @@
 package com.oraclejava.learnjava.fundamentals;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class CollectionsDemo {
 
@@ -10,6 +13,34 @@ public class CollectionsDemo {
 		list1.add("s1");
 		list1.add("s1");
 		System.out.println(list1);
+		
+		List<String> list2 = new ArrayList<>();
+		list2.addAll(list1);
+		System.out.println(list2);
+		
+		Set<String> set1 = new HashSet<>();
+		set1.add("s1");
+		set1.add("s1");
+		set1.add("s2");
+		
+		System.out.println(set1);
+		
+		Iterable<String> list = List.of("s1", "s2", "s3");
+		
+		for (String e: list) {
+			System.out.println(e);
+		}
+		
+		Map<Integer, String> map = Map.of(1, "s1", 2, "s2", 3, "s3");
+		
+		for (Integer key: map.keySet()) {
+			System.out.println(key + ", " + map.get(key));
+		}
+		
+		for (Map.Entry e: map.entrySet()) {
+			System.out.println(e.getKey() + ", " + e.getValue());
+		}
+		
 	}
 
 }
